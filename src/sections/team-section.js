2fedaf0bb7849d008cb6bc8e8f184edd94558946
oldhand7 <oldhanddev@gmail.com>
2024-10-22 01:33:10 +0300

    avatar update

--- src/sections/team-section.js
+++ src/sections/team-section.js
@@ -9,7 +9,6 @@ import { FaFacebookF, FaDumpsterFire, FaInstagram, FaLinkedin } from 'react-icon
 import Member5 from '../assets/team/member-5.png';
 import matt_pfp from '../assets/team/matt_pfp.png';
 import eshan_pfp from '../assets/team/eshan_pfp.png';
-import kris_pfp from '../assets/team/kris_pfp.png';
 import tatsu_pfp from '../assets/team/tatsu_pfp.png';
 import julio_pfp from '../assets/team/julio_pfp.png';
 const data = [
@@ -28,21 +27,7 @@ const data = [
       }
     ],
   },
-  {
-    id: 2,
-    imgSrc: kris_pfp,
-    altText: 'Abigayle Graham',
-    title: 'Abigayle Graham',
-    designation: 'Professional Recruiter',
-    socialProfile: [
-      {
-        id: 1,
-        name: 'linkedin',
-        path: 'https://www.linkedin.com/in/abigayle-graham-453598327/',
-        icon: <FaLinkedin />,
-      }
-    ],
-  },
+
   {
     id: 3,
     imgSrc: matt_pfp,
